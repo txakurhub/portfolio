13516cf150f23c525ec66594ac9757722bd3d4f2
Leandro Pereyra <me.leandrop@gmail.com>
2022-09-05 12:06:58 -0300

    card, ajustes global

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,21 +4,22 @@ import styles from "../styles/Card.module.css";
 export const Card = ({ snapshot, text, deployURL, repo, title, video }) => {
   return (
     <div className={styles.cardContainer}>
+      <div className={styles.cardImg}>
+        <video src={video} type="video/mp4" controls></video>
+        {/* <img src={snapshot} alt="" /> */}
+      </div>
       <div className={styles.cardInfo}>
         <h2 className={styles.cardTitle}>{title}</h2>
-        <div className={styles.cardImg}>
-          <video src={video} type="video/mp4" controls></video>
-          {/* <img src={snapshot} alt="" /> */}
-        </div>
+
         <h4 className={styles.cardText}>{text}</h4>
-      </div>
-      <div className={styles.cardLinks}>
-        <a href={repo}>
-          <ion-icon name="logo-github"></ion-icon>
-        </a>
-        <a href={deployURL}>
-          <ion-icon name="link-outline"></ion-icon>
-        </a>
+        <div className={styles.cardLinks}>
+          <a href={repo}>
+            <ion-icon name="logo-github"></ion-icon>
+          </a>
+          <a href={deployURL}>
+            <ion-icon name="link-outline"></ion-icon>
+          </a>
+        </div>
       </div>
     </div>
   );
