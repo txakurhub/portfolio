d3089ede3c2ee8845de4ef661d7989a0f70546cf
Leandro Pereyra <me.leandrop@gmail.com>
2022-09-22 14:01:17 -0300

    br in about p

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -11,11 +11,11 @@ const About = () => {
           I'm a full-stack developer from Argentina, who enjoys to create
           good-looking content from the aches as working in a team. I've always
           been passionate about technology and when I discovered web
-          development, I realized it would be perfect to me. Recently, I
+          development, I realized it would be perfect to me. <br></br>Recently, I
           graduated from a full-stack coding Bootcamp and want to start
           developing my career in the tech world. An Empathic, enthusiastic
           person, with an assertive communication style and always looking for
-          something new to learn about. I'm currently investigating, working and
+          something new to learn about.<br></br> I'm currently investigating, working and
           of course developing to achive my full potential. If you are
           interested in having a few words, please send me a message.
         </p>
