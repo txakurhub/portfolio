150a51fa6806b217b3f44795311641bd85eedb8b
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-09 12:11:59 -0300

    swiper bullet margin media

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -2,7 +2,7 @@ import { Slider } from "../components/Slider";
 
 const Work = () => {
   return (
-    <section class=" mt-56 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full xl:ml-8">
+    <section class="mt-56 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full xl:ml-8">
       <div class="w-full">
         <h2  class="secondary-title">
           Work
