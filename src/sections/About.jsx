75bdbbeecf317507217569497804f43bbadda827
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-27 18:05:58 -0300

    m & p xl

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,12 +2,12 @@ import IconsTech from "../components/IconsTech";
 
 const About = () => {
   return (
-    <section class="container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full xl:ml-8">
+    <section class="w-full mt-64 pb-10 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24  xl:ml-8">
       <div class="w-full xl:ml-12 mr-12">
         <h2 id="about" class="secondary-title">
           About
         </h2>
-        <p class="section-paragraph">
+        <p class="section-paragraph lg:text-xl">
           I'm a full-stack developer from Argentina, who enjoys to create
           good-looking content from the aches as working in a team. I've always
           been passionate about technology and when I discovered web
@@ -19,7 +19,7 @@ const About = () => {
           of course developing to achive my full potential. If you are
           interested in having a few words, please send me a message.
         </p>
-        <h4 class="my-6 w-full max-w-3xl text-xl font-bold text-selected-text">
+        <h4 class="mb-6 mt-10 w-full max-w-3xl text-2xl font-bold text-selected-text">
           Some of the technologies I work with are
         </h4>
         <IconsTech />
