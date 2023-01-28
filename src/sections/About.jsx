5e19984bc30ce9240d2f044f5863c01915dec200
Leandro Pereyra <me.leandrop@gmail.com>
2023-01-28 02:39:58 -0300

    about

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -8,16 +8,18 @@ const About = () => {
           About
         </h2>
         <p class="section-paragraph lg:text-xl">
-          I'm a full-stack developer from Argentina, who enjoys to create
-          good-looking content from the aches as working in a team. I've always
-          been passionate about technology and when I discovered web
-          development, I realized it would be perfect to me. <br></br>Recently, I
-          graduated from a full-stack coding Bootcamp and want to start
-          developing my career in the tech world. An Empathic, enthusiastic
-          person, with an assertive communication style and always looking for
-          something new to learn about.<br></br> I'm currently investigating, working and
-          of course developing to achive my full potential. If you are
-          interested in having a few words, please send me a message.
+          I am a full-stack developer from Argentina, specialized in creating
+          visually appealing and functional content both working in a team and
+          individually.<br></br>
+          My passion for technology and web development led me to graduate from
+          a full-stack coding bootcamp a year ago and I am focused on continuing
+          to develop my career in the tech industry.<br></br>I am an empathetic,
+          enthusiastic and proactive person, with an assertive communication
+          style and always looking to learn new things.<br></br>I am currently
+          working on interesting projects and developing my skills to reach my
+          full potential.<br></br>
+          If you are interested in collaborating or just chatting, feel free to
+          contact me.
         </p>
         <h4 class="mb-6 mt-10 w-full max-w-3xl text-2xl font-bold text-selected-text">
           Some of the technologies I work with are
@@ -29,3 +31,13 @@ const About = () => {
 };
 
 export default About;
+/* I'm a full-stack developer from Argentina, who enjoys to create
+          good-looking content from the aches as working in a team. I've always
+          been passionate about technology and when I discovered web
+          development, I realized it would be perfect to me. <br></br>A year ago, I
+          graduated from a full-stack coding Bootcamp and want to start
+          developing my career in the tech world.<br></br> An Empathic, enthusiastic
+          person, with an assertive communication style and always looking for
+          something new to learn about.<br></br> I'm currently investigating, working and
+          of course developing to achive my full potential. If you are
+          interested in having a few words, please send me a message. */
