5419e1a73da12c2b18f997d8a9d65093960d40b3
Leandro Pereyra <me.leandrop@gmail.com>
2023-02-01 16:15:14 -0300

    contact

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,25 @@ import Work from "../sections/Work";
 
 const Home = () => {
   function updateList() {
-    const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
-      return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
+    const titles = [...document.querySelectorAll("h1, h2")].sort((a, b) => {
+      return (
+        Math.abs(a.getBoundingClientRect().top) -
+        Math.abs(b.getBoundingClientRect().top)
+      );
     });
-    document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
-    document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
+    document
+      .querySelectorAll(".selected-circle")
+      .forEach((c) => c.classList.remove("selected-circle"));
+    document
+      .querySelectorAll(".nav-dot")
+      [
+        [...document.querySelectorAll("h1, h2")].indexOf(titles[0])
+      ].classList.add("selected-circle");
   }
-  
-  window.addEventListener('scroll', () => {
+
+  window.addEventListener("scroll", () => {
     updateList();
-  })
+  });
   return (
     <main class="mb-24">
       <Presentation />
