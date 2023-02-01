8811ee4f54094ecb95b0ff8b549cbd36b606aa53
Leandro Pereyra <me.leandrop@gmail.com>
2023-02-01 12:20:31 -0300

    descripciones

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -19,7 +19,7 @@ import gravityV from "../assets/gravityV.mp4";
 let sneaker = {
   deployURL: "https://sneakers-xxre.vercel.app",
   repo: "https://github.com/txakurhub/pfinal_client",
-  text: "Application for the purchase of sneakers. Supports payment with QR, MercadoPago and PayPal. It has both a user panel where you can review profile, favorites, shopping cart and purchase history, and an administrator panel with sales statistics, stock, categories and users where to create, search, edit or delete any of the above.",
+  text: "Application for Sneaker Purchase. Supports QR, MercadoPago, and PayPal payments. Features a user panel to review profile, favorites, shopping cart, and purchase history, and an administrative panel with sales statistics, inventory, categories, and users, allowing you to create, search, edit, or delete any of the above.",
   video: sneakV,
   tech: [
     "Javascript",
@@ -39,7 +39,7 @@ let sneaker = {
 let bark = {
   deployURL: "https://barkleaks.vercel.app/",
   repo: "https://github.com/txakurhub/PI-Dogs",
-  text: "A Single Page Application where you can view, search, sort, and filter between more than 500 dog breeds. You can also create, edit or delete your own breed!",
+  text: "Discover the world of canines like never before with BarkLeaks. Browse through information on over 500 breeds, including detailed descriptions, and easily view, search, sort, and filter through them. You can also create, edit, and delete your own breeds.",
   video: barkV,
   tech: [
     "Javascript",
@@ -57,7 +57,7 @@ let bark = {
 let movie = {
   deployURL: "https://moviesapp-one.vercel.app/",
   repo: "https://github.com/txakurhub/movie-app",
-  text: "Web application for a movie catalog showing information on 500+ movies.",
+  text: "MovieApp displays title, genre, and description for over 500+ movies. A simple and straightforward interface for discovering movies.",
   video: movV,
   tech: ["Javascript", "React", "Redux", "CSS", "HTML"],
   snapshot: movieIMG,
@@ -66,7 +66,7 @@ let movie = {
 let globe = {
   deployURL: "https://geosphere.vercel.app/",
   repo: "https://github.com/txakurhub/GeoSphere",
-  text: "Geosphere projection in which we can visualize the data of each country as region, economy, income and population in an interactive way.",
+  text: "Experience data visualization like never before with our Geosphere projection. Explore the regions, economies, incomes, and populations of countries in an interactive and engaging manner.",
   video: geosphereV,
   tech: ["Javascript", "React", "D3", "CSS", "HTML"],
   snapshot: geosphereIMG,
@@ -78,7 +78,7 @@ let gorent = {
     "https://expo.dev/@txakur/go-rent?serviceType=classic&distribution=expo-go",
   video: gorentV,
   repo: "https://github.com/txakurhub/GoRent",
-  text: "Real Estate Mobile App",
+  text: "GoRent streamlines the property buying, renting, and selling process. It offers a user-friendly interface with a vast selection of listings, complete with high-quality photos and detailed descriptions, and powerful search filters.",
   snapshot: gorentIMG,
   tech: [
     "React Native",
@@ -98,7 +98,7 @@ let gorent = {
 let envited = {
   video: "",
   repo: "https://github.com/txakurhub/envited-technical-assessment",
-  text: "Technical Assessment to test front end skill (React js, CSS, HTML), creativity and ability to duplicate Figma designs in a web application, code quality, and attention to detail under time constraints.",
+  text: "The Technical Assessment aims to evaluate your front-end skills (including React.js, CSS, and HTML), creativity, ability to accurately recreate Figma designs within a web application, coding standards, and attention to detail, all within specified time constraints.",
   snapshot: envitedIMG,
   tech: ["Javascript", "React", "TailwindCSS", "Figma", "CSS", "HTML"],
   title: "Envited Challenge",
