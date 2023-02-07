1187f44e43c6b63b3181bf28e1882c97eb6e3c2f
Leandro Pereyra <me.leandrop@gmail.com>
2023-02-06 23:28:42 -0300

    prettier

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -4,11 +4,9 @@ const Work = () => {
   return (
     <section class="mt-56 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full xl:ml-8 ">
       <div class="w-full">
-        <h2  class="secondary-title card">
-          Work
-        </h2>
+        <h2 class="secondary-title card">Work</h2>
         <div id="work" class="card">
-        <Slider />
+          <Slider />
         </div>
       </div>
     </section>
